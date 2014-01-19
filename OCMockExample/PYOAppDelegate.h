6c34ed9e6423117752a9fd634884aefb1133489b
Paul Young <paulyoungonline@gmail.com>
2014-01-19 12:18:51 -0500

    Initial Commit

---
+++ OCMockExample/PYOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PYOAppDelegate.h
+//  OCMockExample
+//
+//  Created by Paul Young on 1/19/14.
+//  Copyright (c) 2014 Paul Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PYOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OCMockExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OCMockExample
+//
+//  Created by Paul Young on 1/19/14.
+//  Copyright (c) 2014 Paul Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PYOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PYOAppDelegate class]));
+    }
+}
